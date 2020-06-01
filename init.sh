3444a793d5f21b3358aab65428bcdf3f3a93dea5
twinkle <st.msu.cmc@gmail.com>
2020-06-02 02:23:09 +0300

    work-work

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 sudo rm /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sites-enabled/test.conf
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+sudo rm /etc/gunicorn.d/test
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart 
