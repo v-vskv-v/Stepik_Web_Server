ea9edc1bec1b57860bb0c1bb0a7bc37a3eb198e7
twinkle <st.msu.cmc@gmail.com>
2020-06-02 15:53:13 +0300

    come on

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-def application(env, start_responce):
-    source = env[QUERY_STRING][2:]
+def WSGIScript(env, start_responce):
+    source = env[QUERY_STRING].split('&')
     start_responce('200 OK', [('Content-Type', 'test/plain')])
-    return bytes(source.replace('&', '\n'))
+    return [token + '\r\n' for token in source]
--- init.sh
+++ init.sh
@@ -1,7 +1,20 @@
-sudo rm /etc/nginx/sites-enabled/default
-sudo rm /etc/nginx/sites-enabled/test.conf
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+if [ -e /etc/nginx/sites-enabled/test.conf ]
+then
+	sudo rm /etc/nginx/sites-enabled/test.conf
+fi
+
+if [ -e /etc/nginx/sites-enabled/default ]
+then 
+	sudo rm /etc/nginx/sites-enabled/default
+fi
+
+sudo ln -s ~/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo rm /etc/gunicorn.d/test
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart 
+
+if [ -e /etc/gunicorn.d/test ]
+then
+	sudo rm /etc/gunicorn.d/test
+fi
+
+sudo ln -s ~/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
