1ea9ce5049297c773026d9cff45a16943b6ba2fc
twinkle <st.msu.cmc@gmail.com>
2020-06-02 16:50:06 +0300

    work-work

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 def WSGIScript(env, start_responce):
-    source = env[QUERY_STRING].split('&')
-    start_responce('200 OK', [('Content-Type', 'test/plain')])
-    return [token + '\r\n' for token in source]
+    source = env['QUERY_STRING'].split('&')
+    start_responce('200 OK', [('Content-Type', 'text/plain')])
+    return ''.join([token + '\n' for token in source])
